92a0525e629d32b92cc673f215b4c5e1a248664e
DavidBolaji <preciouschiefofchrist@yahoo..com>
2023-01-01 19:05:24 +0100

    remove backslash

--- src/Router/homeRouter.js
+++ src/Router/homeRouter.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = new express.Router();
 const homeController = require('../Controller/homeController');
 
-router.get('/api/v1/home', homeController.getHome);
+router.get('api/v1/home', homeController.getHome);
 
 module.exports = router;
